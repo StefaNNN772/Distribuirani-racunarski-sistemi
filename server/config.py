0511ed77271dcec5c77bffe7d78287ecde250d5b
StefaNNN772 <slazarevic772@gmail.com>
2024-11-24 15:21:02 +0100

    Configured Docker - V2

--- server/config.py
+++ server/config.py
@@ -2,7 +2,6 @@ from os import environ
 from datetime import timedelta
 
 class ApplicationConfig:   
-    #SECRET_KEY = os.environ["SECRET_KEY"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = environ.get('DB_URL')

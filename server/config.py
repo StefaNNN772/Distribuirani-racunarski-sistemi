a1cf649b9046bf82bbfcd6dd7f0493e24312df7c
StefaNNN772 <slazarevic772@gmail.com>
2025-07-07 13:28:45 +0200

    Security update

--- server/config.py
+++ server/config.py
@@ -6,12 +6,12 @@ class ApplicationConfig:
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
-    JWT_SECRET_KEY = "slbodvmvs"
+    JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY')
     #Mail configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = 'schneiderelectricinternship@gmail.com'
-    MAIL_PASSWORD = 'aeoo vuqm gifg pnsw'
-    MAIL_DEFAULT_SENDER = 'schneiderelectricinternship@gmail.com'
+    MAIL_USERNAME = environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = environ.get('MAIL_DEFAULT_SENDER')

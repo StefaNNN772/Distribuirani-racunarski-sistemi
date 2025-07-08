1416af9595b15e748bafa54a1490cd080b671f4f
StefaNNN772 <slazarevic772@gmail.com>
2025-07-09 00:52:04 +0200

    Fixed bugs 2

--- server/app.py
+++ server/app.py
@@ -558,4 +558,4 @@ def delete_stocks(id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
